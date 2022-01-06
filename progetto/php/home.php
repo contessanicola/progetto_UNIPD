cf6b58d91955e9c155ae50e51ae6eadfe02f21a3
nicolasalberti00 <75423237+nicolasalberti00@users.noreply.github.com>
2022-01-06 16:41:57 +0100

    Fixato errore di scrittura da 'HTML' ad 'html', in quanto non funzionava altrimenti

--- progetto/php/home.php
+++ progetto/php/home.php
@@ -1,7 +1,7 @@
 <?php
 require_once "functions/importModules.php";
 
-$output = file_get_contents("../HTML/home.html");
+$output = file_get_contents("../html/home.html");
 echo str_replace('<header></header>',importModules::header(),
       str_replace('<aside id="sidebar"></aside>',importModules::sidebar(),
       str_replace('<footer></footer>',importModules::footer(),$output)));
