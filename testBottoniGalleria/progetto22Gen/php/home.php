a65c2f65282b3ac5b3c7a58e62944b180be42e8e
nicolasalberti00 <75423237+nicolasalberti00@users.noreply.github.com>
2022-01-23 00:18:51 +0100

    Rimossa la cartella di test

--- testBottoniGalleria/progetto22Gen/php/home.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once "functions/importModules.php";
-
-$output = file_get_contents("../html/home.html");
-echo str_replace('<header></header>',importModules::header(),
-      str_replace('<aside></aside>',importModules::sidebar(),
-      str_replace('<footer></footer>',importModules::footer(),$output)));
-?>
