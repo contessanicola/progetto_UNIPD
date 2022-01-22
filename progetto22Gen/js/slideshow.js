6b5b6750a7e2452e5e06414a66a199ca14d3c66c
nicolasalberti00 <75423237+nicolasalberti00@users.noreply.github.com>
2022-01-23 00:18:16 +0100

    Modificato sistema visualizzazione immagini galleria annuncio
    Lasciato comunque come backup il sistema di visualizzazione a bottoni precedente, nel caso ci fossero problemi. I file sono stati rinominati con "_bak" alla fine.

--- progetto22Gen/js/slideshow.js
+++ progetto22Gen/js/slideshow.js
@@ -1,26 +1,28 @@
 var slideIndex = 1;
-showDivs(slideIndex);
+showSlides(slideIndex);
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
+// Controlli tasti avanti/indietro
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
 
-function currentDiv(n) {
-  showDivs(slideIndex = n);
+// Controlli immagine thumbnail
+function currentSlide(n) {
+  showSlides(slideIndex = n);
 }
 
-function showDivs(n) {
+function showSlides(n) {
   var i;
-  var x = document.getElementsByClassName("imgCasa");
-  var dots = document.getElementsByClassName("buttonNum");
-  if (n > x.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
+  var slides = document.getElementsByClassName("imgCasa");
+  var dots = document.getElementsByClassName("thumb");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
+  for (i = 0; i < dots.length ; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  x[slideIndex-1].style.display = "block";  
+  slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
