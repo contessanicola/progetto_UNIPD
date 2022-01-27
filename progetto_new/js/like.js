8c111bffdd9cb2f2075c0c9c7dfcdcd5a6b42072
nicolasalberti00 <75423237+nicolasalberti00@users.noreply.github.com>
2022-01-27 20:05:37 +0100

    Fixato bottone "Aggiungi ai Preferiti"

--- progetto_new/js/like.js
+++ progetto_new/js/like.js
@@ -1,17 +1,18 @@
 const heart1 = document.getElementById("favHeart1");
 const heart2 = document.getElementById("favHeart2");
-const button = document.getElementById("buttonLikeText");
+const buttonText = document.getElementById("buttonLikeText");
+const button = document.getElementById("buttonLike");
 
 button.addEventListener("click", function () {
     if (heart1.classList.contains("liked")) {
         heart1.classList.remove("liked");
         heart2.style.display = "none";
         heart1.style.display = "initial";
-        button.innerText = "Tolto dai Preferiti";
+        buttonText.innerText = "Aggiungi ai Preferiti";
       } else {
         heart1.classList.add("liked");
         heart2.style.display = "initial";
         heart1.style.display = "none";
-        button.innerText = "Aggiunto ai Preferiti";
+        buttonText.innerText = "Preferito";
       }
 });
