2f92d9f14cfcec67e086149894315e6924c62c99
Contessa Nicola <nicko96@hotmail.com>
2022-02-01 01:04:27 +0100

    Add files via upload

--- progetto_prefinal/php/catalogo.php
+++ progetto_prefinal/php/catalogo.php
@@ -29,15 +29,41 @@ else{
             if($key == "id_casa" || $key == "regione" || $key == "provincia" || $key == "citta" || $key == "tipologia" || $key == "camere" 
                   || $key == "bagni" || $key == "parcheggio" || $key == "giardino" || $key == "superficie" || $key == "arredato" ||
                   $key == "terrazzo" || $key == "barbecue" || $key == "angolo_bar" || $key == "idromassaggio" || $key == "patio" ){
-                  $filterField[] = $key;
-                  $filterValue[] = $value;
+
+                  if($value != ""){
+                        $filterField[] = $key;
+                        $filterValue[] = $value;
+            }
             }
       }
       $output_validazione_input = true;
+      if(isset($_GET["parcheggio"]))
+            $output = str_replace('value="'.$_GET['parcheggio'].'"','value="'.$_GET['parcheggio'].'"  selected', $output);
+
+      if(isset($_GET["giardino"]))
+            $output = str_replace('value="'.$_GET['giardino'].'"','value="'.$_GET['giardino'].'"  selected', $output);  
+            
+      if(isset($_GET["giardino"]))
+            $output = str_replace('value="'.$_GET['giardino'].'"','value="'.$_GET['giardino'].'"  selected', $output); 
+
+      if(isset($_GET["piscina"]))
+            $output = str_replace('id="piscina"','id="piscina" checked', $output); 
+      if(isset($_GET["idromassaggio"]))
+            $output = str_replace('id="idromassaggio"','id="idromassaggio" checked', $output); 
+      if(isset($_GET["patio"]))
+            $output = str_replace('id="patio"','id="patio" checked', $output);  
+      if(isset($_GET["barbecue"]))
+            $output = str_replace('id="barbecue"','id="barbecue" checked', $output);  
+      if(isset($_GET["angolo_bar"]))
+            $output = str_replace('id="angolo_bar"','id="angolo_bar" checked', $output); 
+      if(isset($_GET["terrazzo"]))
+            $output = str_replace('id="terrazzo"','id="terrazzo" checked', $output); 
+      if(isset($_GET["arredato"]))
+            $output = str_replace('id="arredato"','id="arredato" checked', $output); 
 
       if(isset($_GET['tipologia'])){
 
-            $output = str_replace('value="'.$_GET['tipologia'].'"','value="'.$_GET['tipologia'].'"  checked', $output);
+            $output = str_replace('value="'.$_GET['tipologia'].'"','value="'.$_GET['tipologia'].'"  selected', $output);
             $tipologia=$_GET['tipologia'];           
             $controlli=array();
             $verifica=array();
@@ -77,8 +103,23 @@ else{
             unset($verifica);
             unset($campo);
       }
-      if($output_validazione_input){            
-            $catalogo = $connect->db_getArray(getCasaByMoreField($filterField, $filterValue)); 
+      if($output_validazione_input){  
+            
+            if(count($filterField)!=0){
+                  $aggiunta = "";
+                  if(isset($_GET["prezzoDa"]) && isset($_GET["prezzoA"]))
+                        $aggiunta = ' AND (prezzo BETWEEN '.$_GET["prezzoDa"].' AND '.$_GET["prezzoA"].')';
+
+                  $catalogo = $connect->db_getArray(getCasaByMoreField($filterField, $filterValue).$aggiunta); 
+            }
+            else{
+                  if(isset($_GET["prezzoDa"]) && isset($_GET["prezzoA"])){
+                        $catalogo = $connect->db_getArray('SELECT * FROM casa WHERE (prezzo BETWEEN '.$_GET["prezzoDa"].' AND '.$_GET["prezzoA"].')');
+                  }
+                  else
+                        $catalogo = $connect->db_getArray(getAllCasa());
+            }
+            
       }
       else{
             header('Location: errore.php?errore=Valore+non+valido');     
@@ -122,7 +163,7 @@ if(!empty($catalogo)){
             $temp = str_replace('<div class="camere"></div>', '<div class="camere">'.$casa["camere"].'</div>',$temp);
             $temp = str_replace('<div class="superficie"></div>', '<div class="superficie">'. $casa["superficie"] . '</div>',$temp);
             $temp = str_replace('<div class="bagni"></div>', '<div class="bagni">'.$casa["bagni"].'</div>',$temp);
-            $temp = str_replace('<div class="descrizione"></div>', '<div class="descrizione">'.$casa["descrizione"].'</div>',$temp);
+            $temp = str_replace('<div class="descrizione_casa"></div>', '<div class="descrizione_casa">'.$casa["descrizione"].'</div>',$temp);
             $temp = str_replace('<a href="" class="link_img">', '<a href="dettagli.php?id_casa='.$casa["id_casa"].'" class="link_img">',$temp);
             $temp = str_replace('<a href="" class="contentLinkButton">', '<a href="dettagli.php?id_casa='.$casa["id_casa"].'" class="contentLinkButton">',$temp);
 
